976c06d24f0a895cd2f3b1cf49a55313f885f172
Atiq02 <atiqcseku@gmail.com>
2021-03-01 13:44:47 +0600

    Push

---
+++ dbo/Tables/book.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[book] (
+    [id]               BIGINT        IDENTITY (1, 1) NOT NULL,
+    [created_at]       DATETIME2 (7) NULL,
+    [updated_at]       DATETIME2 (7) NULL,
+    [name]             VARCHAR (255) NULL,
+    [book_category_id] BIGINT        NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FKs8rqq96mvfrfsj9euw5mn973t] FOREIGN KEY ([book_category_id]) REFERENCES [dbo].[book_category] ([id])
+);
+
---
+++ dbo/Tables/book_category.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[book_category] (
+    [id]         BIGINT        IDENTITY (1, 1) NOT NULL,
+    [created_at] DATETIME2 (7) NULL,
+    [updated_at] DATETIME2 (7) NULL,
+    [name]       VARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
