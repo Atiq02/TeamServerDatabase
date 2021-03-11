3d98353f90e89bf021360af3d818e9ce8e053dac
Atiq02 <atiqcseku@gmail.com>
2021-03-11 16:33:11 +0600

    sadsad

--- Database5/dbo/Tables/employee.sql
+++ Database5/dbo/Tables/employee.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [dbo].[employee] (
-    [id]         BIGINT        IDENTITY (1, 1) NOT NULL,
-    [email_id]   VARCHAR (255) NULL,
-    [first_name] VARCHAR (255) NULL,
-    [last_name]  VARCHAR (255) NULL,
-    [Address] VARBINARY(500) NULL, 
-    [test] NVARCHAR(MAX) NULL, 
-    [PermanentAddres] NVARCHAR(MAX) NULL, 
+    [id]              BIGINT          IDENTITY (1, 1) NOT NULL,
+    [email_id]        VARCHAR (255)   NULL,
+    [first_name]      VARCHAR (255)   NULL,
+    [last_name]       VARCHAR (255)   NULL,
+    [Address]         VARBINARY (500) NULL,
+    [test]            NVARCHAR (MAX)  NULL,
+    [PermanentAddres] NVARCHAR (MAX)  NULL,
+    [CurrentAddress]  NVARCHAR (MAX)  NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+
 
