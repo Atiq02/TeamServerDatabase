06de1ba26bbd27c6cf718635f58ea2bb76de83ef
Atiq02 <atiqcseku@gmail.com>
2021-03-14 11:44:32 +0600

    Update book.sql

--- Database5/dbo/Tables/book.sql
+++ Database5/dbo/Tables/book.sql
@@ -3,6 +3,7 @@
     [created_at]       DATETIME2 (7) NULL,
     [updated_at]       DATETIME2 (7) NULL,
     [name]             VARCHAR (255) NULL,
+    [PublishedYear]    VARCHAR (255) NULL,
     [book_category_id] BIGINT        NULL,
     PRIMARY KEY CLUSTERED ([id] ASC),
     CONSTRAINT [FKs8rqq96mvfrfsj9euw5mn973t] FOREIGN KEY ([book_category_id]) REFERENCES [dbo].[book_category] ([id])
