f985d3ff3ef6478988819de06e7193dec7b47532
Atiq02 <atiqcseku@gmail.com>
2021-03-14 11:48:33 +0600

    Update roles.sql

--- Database5/dbo/Tables/roles.sql
+++ Database5/dbo/Tables/roles.sql
@@ -2,6 +2,7 @@
     [id]   INT          IDENTITY (1, 1) NOT NULL,
     [name] VARCHAR (20) NULL,
     [Address] NVARCHAR(MAX) NULL, 
+    [CurrentAddress] NVARCHAR(MAX) NULL, 
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
 
